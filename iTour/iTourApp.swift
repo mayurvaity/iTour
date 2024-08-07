548f1c76660e746c8dd4bec93f31f782cd84230c
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-08-07 22:09:50 +0530

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Mayur Vaity on 07/08/24.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
